9d606aa249bcc5811217d2d2128d84a82921a1d1
Christian Lisangola <jochri3@gmail.com>
2022-06-02 11:52:58 +0300

    1.Créer la base de données

---
+++ clients.sql
@@ -0,0 +1,14 @@
+
+-- Authentification/Connexion
+>mysql -u root -- Si l'utilisateur n'a pas de mot de passe
+>mysql -u root -p -- Si l'utilisateur possède un mot de passe
+
+-- 1. Lister toutes les bases de données
+
+SHOW DATABASES;
+
+-- 2. Création de la base de données
+
+CREATE DATABASE poei_30_mai_ventes;
+
+-- 3. Création de la table clients
