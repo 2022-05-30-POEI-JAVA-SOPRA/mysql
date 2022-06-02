0e0c6772b5795b6c8dd7cda918f7e16357dc589c
Christian Lisangola <jochri3@gmail.com>
2022-06-02 13:01:34 +0300

    4.Création de la table clients

--- clients.sql
+++ clients.sql
@@ -20,3 +20,17 @@ USE poei_30_mai_ventes;
 SHOW TABLES;
 
 -- 4. Création de la table clients
+-- INTEGER : c'est le type entier
+-- NOT NULL : Cette colonne doit toujours contenir une information
+-- AUTO_INCREMENT : Dire à MYSQL de fournir de valeurs en incrementant
+--                  automatiquement les clés
+
+-- PRIMARY KEY : Marquer la colonne comme étant la clé primaire
+-- UNIQUE : Appliquer une contrainte d'unicité
+CREATE TABLE clients(
+ id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ nom VARCHAR(50) NOT NULL,
+ prenom VARCHAR(50) NOT NULL,
+ email VARCHAR(70),
+ telephone VARCHAR(20) NOT NULL
+);
